ed4c624b9a40cf66b5f57eaed82a40537a352ef6
Avinash Dubey <123487788+avidev-creator@users.noreply.github.com>
2024-04-08 07:38:10 +0530

    updated components

--- 11_blogapp/components/index.js
+++ 11_blogapp/components/index.js
@@ -3,5 +3,7 @@ import Footer from "./Footer/Footer"
 import Container from "./container/Container"
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogoutBtn"
+import RTE from "./RTE"
+import PostForm from "./post-form/PostForm"
 
-export {Header, Footer, Container, Logo, LogoutBtn}
+export {Header, PostForm,Footer, RTE, Container, Logo, LogoutBtn}
